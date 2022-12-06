a45657f8e84c031dc0c711d7568d795502aab2e5
Adam Durrant <adamdurrant92@gmail.com>
2022-12-06 18:52:27 +0000

    start redesign

--- js/home/menu-toggle.js
+++ js/home/menu-toggle.js
@@ -1,6 +1,8 @@
 // menu close on click
+document.getElementById("nav-item").addEventListener("click", toggleOff());
+
 function toggleOff() {
-  let toggleButton = document.getElementById("menu-box");
+  let toggleButton = document.getElementById("menu-btn");
   if (toggleButton.checked) {
     toggleButton.checked = false;
   } else {
